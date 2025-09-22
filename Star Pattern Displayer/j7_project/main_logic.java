1227ee52ea28399875bf0dc68c3844b978950f14
sagarpansare1234 <114334021+sagarpansare1234@users.noreply.github.com>
2025-09-22 22:38:15 +0530

    Update main_logic.java
    Function for Filled Square & Right Angled Triangle Done.

--- Star Pattern Displayer/j7_project/main_logic.java
+++ Star Pattern Displayer/j7_project/main_logic.java
@@ -1,1 +1,31 @@
+package star_pattern_displayer;
+
+public class main_logic {
+	//r -> rows   c-> columns
+	/**********************************************************************************************/
+	//Pattern for Filled Square with stars.
+	
+      public void filled_square() {
+    	  System.out.println("\n");
+    	 for(int r=1;r<=5;r++) {
+    		 for(int c=1;c<=5;c++) {
+    			 System.out.print("*");
+    		 }
+    		 System.out.println("");
+    	 }
+    	 System.out.println("");
+      }
+    /**********************************************************************************************/ 
+    //Pattern for Right Angled Triangle 
+      public void rat() {
+    	  System.out.println("\n");
+    	 for(int r=1;r<=5;r++) {
+    		 for(int c=1;c<=r;c++) {
+    			 System.out.print("*");
+    		 }
+    		 System.out.println("");
+    	 }
+    	 System.out.println("");
+      }
+}
 
